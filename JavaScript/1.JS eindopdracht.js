fcd2afe0a676ea4473a46c6c13fb60db7410620e
Sharon <sharon.huijgen@outlook.com>
2021-01-26 12:02:27 +0100

    update js end button input / notes

---
+++ JavaScript/1.JS eindopdracht.js
@@ -0,0 +1,7 @@
+
+function sb_7(){
+    // alert("Linked JS doc and knop 7")
+    // let button_7 = parseInt(document.getElementById("rkm_scherm")).value;
+    let button_7 = 7;
+    console.log(button_7);
+}
