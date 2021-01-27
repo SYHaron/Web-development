002af10e1a1c52458cdd0efe0825a1799d5d65e5
Sharon <sharon.huijgen@outlook.com>
2021-01-27 12:00:58 +0100

    update js end rkms

--- JavaScript/1.JS eindopdracht rekenmachine big.js
+++ JavaScript/1.JS eindopdracht rekenmachine big.js
@@ -4,56 +4,56 @@
 
 
 // Functions cijfers 1 t/m 9
-function sb_0(){
-    number = 0;
-     console.log(number);
-     document.getElementById("rkm_scherm").innerHTML = number;
- }
-function sb_1(){
-    number = 1;
-     console.log(number);
-     document.getElementById("rkm_scherm").innerHTML = number;
- }
-function sb_2(){
-    number = 2;
-     console.log(number);
-     document.getElementById("rkm_scherm").innerHTML = number;
- }
-function sb_3(){
-    number = 3;
-     console.log(number);
-     document.getElementById("rkm_scherm").innerHTML = number;
- }
-function sb_4(){
-    number = 4;
-     console.log(number);
-     document.getElementById("rkm_scherm").innerHTML = number;
- }
-function sb_5(){
-    number = 5;
-     console.log(number);
-     document.getElementById("rkm_scherm").innerHTML = number;
- }
-function sb_6(){
-    number = 6;
-     console.log(number);
-     document.getElementById("rkm_scherm").innerHTML = number;
- }
-function sb_7(){
-    // alert("Linked JS doc and knop 7")
-    // let button_7 = parseInt(document.getElementById("rkm_scherm")).value;
-   number = 7;
-    console.log(number);
-    document.getElementById("rkm_scherm").innerHTML = number;
-}
-function sb_8(){
-   number = 8;
-    console.log(number);
-    document.getElementById("rkm_scherm").innerHTML = number;
-}
-function sb_9(){
-    number = 9;
-     console.log(number);
-     document.getElementById("rkm_scherm").innerHTML = number;
- }
+// function sb_0(){
+//     number = 0;
+//      console.log(number);
+//      document.getElementById("rkm_scherm").innerHTML = number;
+//  }
+// function sb_1(){
+//     number = 1;
+//      console.log(number);
+//      document.getElementById("rkm_scherm").innerHTML = number;
+//  }
+// function sb_2(){
+//     number = 2;
+//      console.log(number);
+//      document.getElementById("rkm_scherm").innerHTML = number;
+//  }
+// function sb_3(){
+//     number = 3;
+//      console.log(number);
+//      document.getElementById("rkm_scherm").innerHTML = number;
+//  }
+// function sb_4(){
+//     number = 4;
+//      console.log(number);
+//      document.getElementById("rkm_scherm").innerHTML = number;
+//  }
+// function sb_5(){
+//     number = 5;
+//      console.log(number);
+//      document.getElementById("rkm_scherm").innerHTML = number;
+//  }
+// function sb_6(){
+//     number = 6;
+//      console.log(number);
+//      document.getElementById("rkm_scherm").innerHTML = number;
+//  }
+// function sb_7(){
+//     // alert("Linked JS doc and knop 7")
+//     // let button_7 = parseInt(document.getElementById("rkm_scherm")).value;
+//    number = 7;
+//     console.log(number);
+//     document.getElementById("rkm_scherm").innerHTML = number;
+// }
+// function sb_8(){
+//    number = 8;
+//     console.log(number);
+//     document.getElementById("rkm_scherm").innerHTML = number;
+// }
+// function sb_9(){
+//     number = 9;
+//      console.log(number);
+//      document.getElementById("rkm_scherm").innerHTML = number;
+// }
 
---
+++ JavaScript/1.JS eindopdracht rekenmachine small.js
@@ -0,0 +1,14 @@
+let getal1 = parseInt(document.getElementById("rkm_smallNr1"));
+let getal2 = parseInt(document.getElementById("rkm_smallNr2"));
+let output = document.getElementById("rkm_smallScherm");
+
+function b_plus(waarde) {
+    // let plus = getal1 + getal2;
+    console.log(waarde.innerText);
+}
+
+// = teken
+function b_calc() {
+    let plus = eval(getal1 + getal2);
+
+}
